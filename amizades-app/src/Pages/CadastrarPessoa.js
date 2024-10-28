deb9de993376de90f54f3bde1a60a69b19a82f47
LipeAKugel <felipearaujodff@gmail.com>
2024-10-28 15:51:54 -0300

    criando formulário de criar pessoa
    Co-authored-by: João Vitor <Jauzimm@users.noreply.github.com>

--- amizades-app/src/Pages/CadastrarPessoa.js
+++ amizades-app/src/Pages/CadastrarPessoa.js
@@ -1,8 +1,42 @@
+import { useState } from "react";
+import '../Styles/pessoa.css';
 
 function CadastrarPessoa() {
+
+  const [nome, setNome] = useState("");
+  const [idade, setIdade] = useState("");
+  const [ocupacao, setOcupacao] = useState("");
+
   return (
-    <div>
-      CadastrarPessoa
+    <div className="pessoa-container">
+      <form className="pessoa-form">
+        <h2>Cadastrar pessoa</h2>
+        <label>
+          Nome:
+          <input 
+            type="text" 
+            value={nome} 
+            onChange={(e) => setNome(e.target.value)} 
+          />
+        </label>
+        <label>
+          Idade:
+          <input
+            type="text"
+            value={idade}
+            onChange={(e) => setIdade(e.target.value)}
+          />
+        </label>
+        <label>
+          Ocupação:
+          <input 
+            type="text"
+            value={ocupacao}
+            onChange={(e) => setOcupacao(e.target.value)}
+          />
+        </label>
+        <button>Cadastrar</button>
+      </form>
     </div>
   );
 }
