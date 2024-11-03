89e022c9e65a9b29f34b791b399d7686593a2319
Jauzimm <joaovitorso071@gmail.com>
2024-11-03 14:08:28 -0300

    trabalhando em criarRelacao

--- amizades-app/src/Pages/CriarRelacao.js
+++ amizades-app/src/Pages/CriarRelacao.js
@@ -1,7 +1,15 @@
+import '../Styles/relacao.css';
+import { useState } from "react";
+
 function CriarRelacao() {
+  const [isActive, setIsActive] = useState(false);
   return (
-    <div>
-      CriarRelacao
+    <div className="dropdown">
+      <div className="dropdown-btn" onClick={() => setIsActive(!isActive)}>Escolha</div>
+      <div className={`dropdown-content ${isActive ? "active" : ""}`}>
+        <div className="dropdown-item">Felipe</div>
+        <div className="dropdown-item">João</div>
+      </div>
     </div>
   );
 }
